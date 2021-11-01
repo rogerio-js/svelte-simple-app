b0dde420eb5e28cdc22cf0c6aa10502f131a2ffb
Rogério Menezes <roger@rogermenezes.com>
2021-11-01 18:12:15 +1300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1 class=" bg-yellow-500">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<br>
+	<h3 class="text-purple-900 bg-blue-100 p-4">1Svelte and Tailwind Starter Template</h3>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
